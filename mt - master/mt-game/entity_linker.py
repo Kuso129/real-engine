f649eeea89bf895389e02123a5ae3438d2a83b80
Kuso129 <108622572+Kuso129@users.noreply.github.com>
2022-07-19 19:07:03 +0300

    Delete mt - master directory

--- mt - master/mt-game/entity_linker.py
+++
@@ -1,9 +0,0 @@
-from first_person_camera import*
-from dplayer import*
-from funk_brush import*
-# from ibase_skybox import*
-
-def link_all_entitys():
-    link_entity(defoult_fps_camera, 'info_first_person_camera')
-    link_entity(demo_game_player, 'info_demo_player')
-    # link_entity(def_skybox, 'r_skybox')
--- mt - master/mt-game/main.py
+++
@@ -1,7 +0,0 @@
-from programm import*
-from entity_linker import*
-
-if __name__ == '__main__':
-    curent_programm = Programm()
-    link_all_entitys()
-    curent_programm.run()
